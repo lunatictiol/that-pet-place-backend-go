697ac04a7aa94b449b614db136caf0474fca335e
“sabiq” <sabiq.a@myclnq.co.in>
2024-08-27 12:20:07 +0530

    types config

---
+++ types/types.go
@@ -0,0 +1,28 @@
+package types
+
+import "time"
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName" validate:"required"`
+	LastName  string `json:"lastName" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=5,max=8" `
+}
+type LoginUserPayload struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required" `
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+type UserStore interface {
+	FindUserByEmail(email string) (*User, error)
+	FindUserById(id int) (*User, error)
+	CreateUser(User) error
+}
